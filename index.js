e06dd238fb9c1e5559392226217bea7e3e9238ac
#EightSeven <hasheightseven@gmail.com>
2024-12-19 12:25:30 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const neonmonkey = document.getElementById('neon-monkey-popeye');
-
-neonmonkey.textContent = 'neon monkey'.toUpperCase();
-neonmonkey.style.backgroundColor = '#00192887';
-neonmonkey.style.color = '#abcdef87';
-neonmonkey.style.width = window.availWidth;
-neonmonkey.style.height = window.availHeight;
