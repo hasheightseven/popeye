4038f94c9fcfab899dbcf3e6045f7f766d80fee0
#EightSeven <hasheightseven@gmail.com>
2024-12-19 11:40:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ export const neonmonkey = document.getElementById('neon-monkey-popeye');
 
 neonmonkey.addEventListener('load', () => {
   this.width = window.screenWidth();
+  this.style.backgroundColor = '#001928';
 });
