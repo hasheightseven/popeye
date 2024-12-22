427edc6d506b8726b5696e17ae6b7c87a4004d9c
#EightSeven <hasheightseven@gmail.com>
2024-12-22 17:35:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 
 export default function App() {
     return (
-        <div classname='App'>ello mastartu</div>
+        <div classname='App'>
+            <svg xmlns='https://www.w3.org/2000/svg/'>
+                <path d='M126 126 L269 269 L126 269 Z' fill='abcdef'></path>
+            </svg>
+        </div>
     )
 }
