9ce398aef4d4704fda3f72d1525fdb3a0bcf82a3
#EightSeven <hasheightseven@gmail.com>
2024-12-25 19:03:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <div classname='App'>
             <svg xmlns='https://www.w3.org/2000/svg/'>
-                <path d='M126 126 L269 269 L126 269 Z' fill='abcdef'></path>
+                <path d='M126 126 L269 269 L126 269 Z' fill='#abcdef'></path>
             </svg>
         </div>
     )
