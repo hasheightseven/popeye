b00e783433647d48354f9bda27ae7ccf2764e357
#EightSeven <hasheightseven@gmail.com>
2024-12-26 14:09:08 +0530

    Create SVGLogoElement.js

---
+++ src/SVGLogoElement.js
@@ -0,0 +1,9 @@
+import React import 'react';
+
+export default const SVGLogoElement = () => {
+  return (
+    <svg xmlns="www.w3.org/2000/svg" width="300px" height="300px">
+      <path d="M5050L15050L150150Z" fill="#001928"></path>
+    </svg>
+  )
+};
