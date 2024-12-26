dfa476bf7c5133a45a43f76d87bf250ab0e6a3a0
#EightSeven <hasheightseven@gmail.com>
2024-12-26 15:26:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('neon-monkey-popeye'));
 root.render(
   <React.StrictMode>
     <App />
