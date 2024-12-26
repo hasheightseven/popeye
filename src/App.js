998c80efb74f4b29586bb4efa39c6b0a7eb89113
#EightSeven <hasheightseven@gmail.com>
2024-12-26 18:16:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import SVGLogoElement from "./SVGLogoElement.js";
 function App() {
     return (
         <main className=".App">
-            <nav className=".AppNavbar">
-                <SVGLogoElement />
-            </nav>
-            <div className=".AppDiv">
-                <h2 className=".AppH2">welcome to the jungle</h2>
+            <div className=".AppDiv" style={{
+                background-color: #92112608;
+                
+                }}>
+                <nav className=".AppNavbar">
+                    <SVGLogoElement />
+                </nav>
+                
+                <div className=".AppDiv">
+                    <h2 className=".AppH2">welcome to the jungle</h2>
+                </div>
             </div>
         </main>
     );
