9c597c754af3bd91cb2ac351e2fece53a548a4f2
#EightSeven <hasheightseven@gmail.com>
2024-12-26 15:35:25 +0530

    Update SVGLogoElement.js

--- src/SVGLogoElement.js
+++ src/SVGLogoElement.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SVGLogoElement() {
   return (
     <svg xmlns="www.w3.org/2000/svg" width="300px" height="300px">
-      <path d="M5050L15050L150150Z" fill="#001928"></path>
+      <path d="M 50 50 L 150 50 L 150 150 Z" fill="#001928"></path>
     </svg>
   )
 };
