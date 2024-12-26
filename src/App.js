a27382c2935e3838bee4acc81bcb90f083043e4c
#EightSeven <hasheightseven@gmail.com>
2024-12-26 16:44:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import SVGLogoElement from "./SVGLogoElement.js";
 function App() {
     return (
         <main className=".App">
-            <SVGLogoElement />
+            <nav className=".AppNavbar">
+                <SVGLogoElement />
+            </nav>
+            <div className=".AppDiv">
+                <h2 className=".AppH2">welcome to the jungle</h2>
+            </div>
         </main>
     );
 };
