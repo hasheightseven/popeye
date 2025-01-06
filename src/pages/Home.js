b673ea9bc055fbc29a30862244a3f9a61bafa532
#EightSeven <hasheightseven@gmail.com>
2025-01-06 15:15:27 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="AppHome">
+            <p className="AppParagraph">welcome!</p>
+            <p className="AppParagraph">
+                hi I am 
+                <h1 className="AppLogoHeading">popeye the dev</h1>
+            </p>
+        </div>
