067119d96f7593dfad9e2aef543eb61f1ca56c99
#EightSeven <hasheightseven@gmail.com>
2025-01-21 17:54:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 //import SVGLogoElement from "./SVGLogoElement.js";
-
+import "./LogoNM.js";
 function App() {
     return (
         <main className="App">
-            <div className="AppOverlay"></div>
+            <div className="AppOverlay">
+                <LogoNM />
+            </div>
         </main>
     );
 };
