2f3d1b3fc6482f604b3320233a0f9881a085d2ae
#EightSeven <hasheightseven@gmail.com>
2025-01-22 22:04:24 +0530

    Create LandingPopeye.js

---
+++ src/components/LandingPopeye.js
@@ -0,0 +1,15 @@
+function LandingPopeye() {
+    return (
+        <div className="landing-popeye">
+            <div className="btn-landing-login">login</div>
+            <div className="form-landing-contact">
+                <form for="form-landing-contact">
+                    <input type="text/plain" placeholder="enter your name" id="landing-popeye-name" required>
+                    <button type="submit"></button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPopeye;
