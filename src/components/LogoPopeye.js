70d076d9221d2a91ef0afd9b558fce5ea87f074b
#EightSeven <hasheightseven@gmail.com>
2025-01-22 21:06:51 +0530

    Create LogoPopeye.js

---
+++ src/components/LogoPopeye.js
@@ -0,0 +1,12 @@
+function LogoPopeye() {
+    return (
+        <div id="logo-popeye">
+            <div id="logo-line-0"></div>
+            <div id="logo-line-1"></div>
+            <div id="logo-line-2"></div>
+            <div id="logo-line-3"></div>
+        </div>
+    );
+}
+
+export default LogoPopeye;
