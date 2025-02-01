95bc7d286662501e0041ecba30d91887671021f2
#EightSeven <hasheightseven@gmail.com>
2025-02-01 15:35:48 +0530

    Create BottomMenu.js

---
+++ src/components/BottomMenu.js
@@ -0,0 +1,14 @@
+const BottomMenu = () => {
+    let menuitems = { };
+    
+    
+    return (
+        <div className="BottomMenu">
+            <ul className="menu-items">
+                
+            </ul>
+        </div>
+    );
+}
+
+export default BottomMenu;
