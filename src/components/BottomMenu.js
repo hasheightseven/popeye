8a0823c55bf7e727b8be9eead7ca2b62b24c1b0f
#EightSeven <hasheightseven@gmail.com>
2025-02-01 16:33:52 +0530

    Update BottomMenu.js

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -1,14 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaHome, FaSearch, FaPlusCircle, FaBell, FaUser } from "react-icons/fa";
+import "./BottomMenu.css"; // Import CSS for styling
+
 const BottomMenu = () => {
-    let menuitems = { };
-    
-    
-    return (
-        <div className="BottomMenu">
-            <ul className="menu-items">
-                {{ menuitems }}
-            </ul>
-        </div>
-    );
-}
+  return (
+    <nav className="bottom-menu">
+      <NavLink to="/" className="menu-item">
+        <FaHome />
+        <span>Home</span>
+      </NavLink>
+      <NavLink to="/search" className="menu-item">
+        <FaSearch />
+        <span>Search</span>
+      </NavLink>
+      <NavLink to="/add" className="menu-item">
+        <FaPlusCircle />
+        <span>Add</span>
+      </NavLink>
+      <NavLink to="/notifications" className="menu-item">
+        <FaBell />
+        <span>Alerts</span>
+      </NavLink>
+      <NavLink to="/profile" className="menu-item">
+        <FaUser />
+        <span>Profile</span>
+      </NavLink>
+    </nav>
+  );
+};
 
 export default BottomMenu;
