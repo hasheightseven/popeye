1442990d4c668acc005706597f9a88611b10a965
#EightSeven <hasheightseven@gmail.com>
2025-02-01 16:36:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/*
+
 import "./App.css";
 //import SVGLogoElement from "./SVGLogoElement.js";
 import Home from "./pages/Home.js";
@@ -10,6 +12,32 @@ function App() {
             </div>
         </main>
     );
+};
+
+export default App;
+
+
+*/
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BottomMenu from "./BottomMenu";
+
+const App = () => {
+  return (
+    <Router>
+      <div style={{ paddingBottom: "60px" }}>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/search" element={<h1>Search Page</h1>} />
+          <Route path="/add" element={<h1>Add New Item</h1>} />
+          <Route path="/notifications" element={<h1>Alerts</h1>} />
+          <Route path="/profile" element={<h1>Profile Page</h1>} />
+        </Routes>
+        <BottomMenu />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
