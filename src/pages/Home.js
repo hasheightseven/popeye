8a00ca1c85114bef638686d19b8fad3704eaf594
#EightSeven <hasheightseven@gmail.com>
2025-02-01 15:36:55 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import "../App.css";
 import LogoPopeye from "../components/LogoPopeye.js";
+import BottomMenu from "./components/BottomMenu.js";
 
 function Home() {
     return (
         <div className="AppHome">
             <LogoPopeye />
+            <BottomMenu />
         </div>
     );
 }
